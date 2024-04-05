ea92b8e18b8a8705ce4c4daabc2eec0df7c0c353
Arod39 <arodriguez39@hawk.iit.edu>
2024-04-04 20:37:08 -0500

    Add Final JS
    I spent time understanding the final code and comparing it to what was covered in the lecture where Event Handling was covered.
    
    I can explain each line and will do so, just for my understanding within the readme file.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
-//add your javascrip between these two lines of code
+ var button1 = document.getElementById('entrybutton');
+ var usertext = document.getElementById('entryinput');
  
-
-
-
-
-
-
-
+ function showtext() {
+  document.getElementById('textoutput').innerHTML = usertext.value;
+  alert("Andrew Rodriguez: " + usertext.value);
+ }
+ 
+ button1.addEventListener('click', showtext);
+}
 window.addEventListener('load', init);
